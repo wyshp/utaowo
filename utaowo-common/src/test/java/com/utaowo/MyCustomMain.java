17929fb5f482c275de9762755e7e523feb1a1ac4
wuys <wuys@yingkelecai.com>
2019-05-13 18:34:19 +0800

    项目初始化

---
+++ utaowo-common/src/test/java/com/utaowo/MyCustomMain.java
@@ -0,0 +1,15 @@
+package com.utaowo;
+
+/**
+ * <p>
+ *
+ * @AUTHOR WYS
+ * @DATE 2019-05-13
+ * @VERSION v1.0
+ */
+public class MyCustomMain {
+
+    public static void main(String[] args){
+        System.out.println("this is test ....");
+    }
+}
